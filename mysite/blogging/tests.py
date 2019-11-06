c2a3bf80ce0abe26003fe4bc131c793c8a66591e
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-05 19:56:45 -0800

    Added front end tests

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils.timezone import utc
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post
@@ -32,3 +34,36 @@ class CategoryTestCase(TestCase):
         actual = str(c1)
 
         self.assertEqual(expected, actual)
+
+
+class FrontEndTestCase(TestCase):
+    """ Test views provided in the front end """
+
+    fixtures = ['blogging_test_fixture.json']
+
+    def setup(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+
+        author = User.objects.get(pk=1)
+
+        for count in range(1, 11):
+            post = Post(title='Post %d Title' % count, text='foo', author='author')
+            if count < 6:
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue('Recent Posts' in resp_text)
+
+        for count in range(1, 11):
+            title = 'Post %d Title' % count
+            if count < 1:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
