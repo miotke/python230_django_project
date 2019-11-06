a32de880a2215f320219d19a8c7aa57c9e09f6cc
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-05 19:12:03 -0800

    Created a stub view

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, Http404
 
-# Create your views here.
+
+def stub_view(request, *args, **kwargs):
+    body = 'Stub view\n\n'
+    if args:
+        body += 'Args:\n'
+        body += '\n'.join(['\t%s' % a for a in args])
+    if kwargs:
+        body += 'Kwargs:\n'
+        body += '\n'.join(['\t%s' % i for i in kwargs])
+
+    return HttpResponse(body, content_type='text/plain')
