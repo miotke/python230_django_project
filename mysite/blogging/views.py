a5f1f6e2075e256f860dcb6d051a812a83e7380e
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-05 19:57:42 -0800

    Rendering list view

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, Http404
+from django.template import loader
+from blogging.models import Post
 
 
 def stub_view(request, *args, **kwargs):
@@ -12,3 +14,13 @@ def stub_view(request, *args, **kwargs):
         body += '\n'.join(['\t%s' % i for i in kwargs])
 
     return HttpResponse(body, content_type='text/plain')
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+
+    return HttpResponse(body, content_type='text/html')
