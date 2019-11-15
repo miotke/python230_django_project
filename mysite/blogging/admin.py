adc87514630d9452c6cca7e79491a0cfc0ee0031
Andrew Miotke <andrewmiotke@gmail.com>
2019-11-14 19:07:55 -0800

    Added the categories to the posts for assignment

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
-admin.site.register(Post)
+# admin.site.register(Post)
 
 
 
-class PostCategory(admin.ModelAdmin):
-    admin.site.register(Category)
+# class PostCategory(admin.ModelAdmin):
+#     admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+# @admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
